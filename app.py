d237b4c7810330f516f1fd2604c78502ad7ba434
satyazmx <thakursatya6@gmail.com>
2022-07-12 15:30:45 +0530

    add logger and exception module

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import sys
 from flask import Flask
-
+from sharing.logger import logging
+from sharing.exception import SharingException
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("Testing Exception  module")
+    except Exception as e:
+        sharing = SharingException(e,sys)
+        logging.info(sharing.error_message)
+    logging.info("Testing logger module")
     return "CI CD pipeline has been established."
 
 
