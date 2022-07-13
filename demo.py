5b7b9008f0fee24f1b0887943febf92f50596490
satyazmx <thakursatya6@gmail.com>
2022-07-13 23:44:33 +0530

    add DATA VALIDATION component

--- demo.py
+++ demo.py
@@ -1,19 +1,22 @@
 from distutils.command.config import config
 from sharing.config.configuration import *
 from sharing.component.data_ingestion import DataIngestion
+from sharing.component.data_validation import DataValidation
 from sharing.entity.config_entity import DataValidationConfig
+from sharing.entity.artifact_entity import DataIngestionArtifact
 from sharing.pipeline.pipeline import Pipeline
 import logging
 
 def main():
     try:
-        #pipe = Pipeline()
-        #pipe.run_pipeline()
-        config = Configuartion().get_data_validation_config()
-        print(config)
-        
+        pipe = Pipeline()
+        pipe.run_pipeline()
+        #config = Configuartion().get_data_validation_config()
+        #print(config)
         #ingested = DataIngestion(data_ingestion_config=DataIngestionConfig)
         #ingested.download_sharing_data()
+    
+        
         
     except Exception as e:
         logging.error(f"{e}")
--- sharing/pipeline/pipeline.py
+++ sharing/pipeline/pipeline.py
@@ -1,10 +1,12 @@
+from http.client import ImproperConnectionState
 import os, sys
 from sharing.logger import logging
 from sharing.exception import SharingException
 from sharing.config.configuration import Configuartion
-from sharing.entity.config_entity import DataIngestionConfig
+from sharing.entity.config_entity import *
 from sharing.entity.artifact_entity import DataIngestionArtifact
 from sharing.component.data_ingestion import DataIngestion
+from sharing.component.data_validation import DataValidation
 
 class Pipeline:
     
@@ -22,8 +24,12 @@ class Pipeline:
         except Exception as e:
             raise SharingException(e,sys) from e
     
-    def start_data_validation(self):
-        pass
+    def start_data_validation(self,data_ingestion_artifact: DataIngestionArtifact):
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),data_ingestion_artifact=data_ingestion_artifact)
+            data_validation.initiate_data_validation()
+        except Exception as e:
+            raise SharingException(e,sys) from e 
 
     def start_data_transformation(self):
         pass
@@ -42,7 +48,7 @@ class Pipeline:
             #data ingestion
 
             data_ingestion_artifact = self.start_data_ingestion()
-            
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
 
         except Exception as e:
             raise SharingException(e,sys) from e
