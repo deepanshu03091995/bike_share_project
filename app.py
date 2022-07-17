f9a7a2446a8a079aeaa795beb46898a5c5fdd4dc
satyazmx <thakursatya6@gmail.com>
2022-07-17 18:25:03 +0530

    Add index.html template

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import sys
-from flask import Flask
+import sys,pip,os,json
+
+from flask import Flask,send_file, abort, render_template
 from sharing.logger import logging
 from sharing.exception import SharingException
+from sharing.config.configuration import Configuartion
+from sharing.constant import CONFIG_DIR, get_current_time_stamp
+from sharing.pipeline.pipeline import Pipeline
+
+
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
     try:
-        raise Exception("Testing Exception  module")
+        return render_template('index.html')
     except Exception as e:
         sharing = SharingException(e,sys)
         logging.info(sharing.error_message)
